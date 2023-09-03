3929a16ab7d64e6dbf248890bc53d4b5465eee41
Alfaz-Ahmad <120320595+Alfaz-Ahmad@users.noreply.github.com>
2023-09-03 18:53:37 +0530

    Add files via upload

---
+++ [H]input_and_output.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int x, y, z;
+    cin >> x;
+    cin >> y;
+    cin >> z;
+    cout << x+y+z << endl;
+    return 0;
+}
+
